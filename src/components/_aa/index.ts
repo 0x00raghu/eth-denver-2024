aa143dc11032cf8b9cd6dcac6b994614db3d74a6
Raghu <161108407+0x00raghu@users.noreply.github.com>
2024-02-28 23:56:41 +0530

    fix: Metamask error

--- src/components/_aa/index.ts
+++ src/components/_aa/index.ts
@@ -1,51 +1,25 @@
 import { createModularAccountAlchemyClient } from '@alchemy/aa-alchemy';
-import { LocalAccountSigner, baseSepolia, type Hex, Address } from '@alchemy/aa-core';
 import { createWalletClient, custom } from 'viem';
-import { mainnet, WalletClientSigner } from '@alchemy/aa-core';
-// import { WalletClientSigner } from "@alchemy/core";
-
-const client = createWalletClient({
-  chain: mainnet,
-  transport: custom(window.ethereum),
-});
-
-// this can now be used as an signer for a Smart Contract Account
-export const eoaSigner = new WalletClientSigner(
-  client,
-  'json-rpc', //signerType
-);
-
-const chain = baseSepolia; // arbiSepolia
-
-// The private key of your EOA that will be the signer to connect with the Modular Account
-// Our recommendation is to store the private key in an environment variable
-const PRIVATE_KEY = process.env.NEXT_PUBLIC_PRIVATE_KEY as Hex;
-console.log('PRIVATE_KEY: ', PRIVATE_KEY);
-// const signer = LocalAccountSigner?.privateKeyToAccountSigner(PRIVATE_KEY);
-
-// Create a smart account client to send user operations from your smart account
-const createClient = async () => {
-  //   // Fund your account address with ETH to send for the user operations
-  //   const client = await createModularAccountAlchemyClient({
-  //     // get your Alchemy API key at https://dashboard.alchemy.com
-  //     apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY as string,
-  //     chain,
-  //     signer,
-  //   });
-  //   // (e.g. Get Sepolia ETH at https://sepoliafaucet.com)
-  //   console.log('Smart Account Address: ', client.getAddress()); // Log the smart account address
-};
+import { baseSepolia, WalletClientSigner } from '@alchemy/aa-core';
+import { Address } from '@alchemy/aa-core';
 
 const sendUserOperation = async () => {
+  console.log(window.ethereum, 'window.ethereum');
+  const chain = baseSepolia;
+  const client = createWalletClient({
+    chain,
+    transport: custom(window.ethereum),
+  });
+
+  const eoaSigner = new WalletClientSigner(client, 'json-rpc');
+
   const provider = await createModularAccountAlchemyClient({
-    // get your Alchemy API key at https://dashboard.alchemy.com
     apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY,
     chain,
     signer: eoaSigner,
   });
-  // Fund your account address with ETH to send for the user operations
-  // (e.g. Get Sepolia ETH at https://sepoliafaucet.com)
-  console.log('Smart Account Address: ', provider.getAddress()); // Log the smart account address
+
+  console.log('Smart Account Address: ', provider.getAddress());
 
   const shivaAddress = '0x5B4d77e199FE8e5090009C72d2a5581C74FEbE89' as Address;
   // Send a user operation from your smart account to Vitalik that does nothing
@@ -67,4 +41,4 @@ const sendUserOperation = async () => {
   console.log('Transaction Hash: ', txHash); // Log the transaction hash
 };
 
-export { createClient, sendUserOperation };
+export { sendUserOperation };
